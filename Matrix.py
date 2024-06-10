aa3ca285f8514a33e7f5c51e17ea9dd667263552
kounet95 <85202452+kounet95@users.noreply.github.com>
2024-06-10 11:39:07 -0400

    oumar

--- Matrix.py
+++ Matrix.py
@@ -1,14 +1,23 @@
 import Array2D
 
 class Matrix:
-    def __init__(self, l, c):
+    def __init__(self, l=0, c=0):
         self.nRows = l
         self.nCols = c
         self.values = Array2D.Array2D(l, c)
-
+    def nRows(self):
+        nombredeLigne = self.nRows
+        return nombredeLigne
+    def nCols(sel):
+        nombredeColone = sel.nCols
+        return nombredeColone
+    
     def print(self):
         self.values.print()
 
 m = Matrix(4, 3)
 m.print()
-    
+print("********************")
+print("le nombre de ligne est:", m.nRows)  
+print("********************")
+print("le nombre de colone est:", m.nCols)  
