1ae7369f988bd4b856076c7f5861d78e10d95419
kounet95 <85202452+kounet95@users.noreply.github.com>
2024-06-17 16:34:47 -0400

    comm

--- Matrix.py
+++ Matrix.py
@@ -1,7 +1,7 @@
 import Array2D
 
 class Matrix:
-    def __init__(self, l= 0, c= 0):
+    def __init__(self, l, c):
         self.nRows = l
         self.nCols = c
         self.values = Array2D.Array2D(l, c)
